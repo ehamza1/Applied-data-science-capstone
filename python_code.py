bbcb38f1e95aab2811283287f54ad64c9e8161b9
ehamza1 <ehab_hamza11@yahoo.com>
2020-05-09 19:12:58 -0600

    script

---
+++ python_code.py
@@ -0,0 +1,12 @@
+{
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "## Introduction\n",
+    "\n",
+    "this notebook will be mainly used for the capstone project."
+    ]
+  },
+  import pandas as pd
+  import numpy as np
+  print("Hello Capstone Project Course!")
